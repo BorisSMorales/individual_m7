e8c075aea8a649cf531f09f9856338355b42c567
BorisSMorales <salvadormoralesacuna@gmail.com>
2023-07-03 02:17:51 -0400

    Modelos, manejo de Etiqueta en Admin y migraciones

--- individual_m7app/admin.py
+++ individual_m7app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Etiqueta
 
 # Register your models here.
+
+@admin.register(Etiqueta)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
--- individual_m7app/models.py
+++ individual_m7app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Etiqueta(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('pendiente', 'Pendiente'),
+        ('en_progreso', 'En progreso'),
+        ('completada', 'Completada'),
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pendiente')
+    tag = models.ForeignKey(Etiqueta, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
